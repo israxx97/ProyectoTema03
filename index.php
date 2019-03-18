3d24b0563ebf4458b99db5171913b010379320f3
israxx97 <isragarcia97@gmail.com>
2019-03-18 15:04:05 +0100

    + v2.1 - 18/03/2019, 15:03 + Añadido index.php.

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="es-eu" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Mto Departamentos Tema 4 - Israel García Cabañeros</title>
+        <link rel="stylesheet" type="text/css" href="./webroot/css/styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark justify-content-between">
+            <a class="navbar-brand" href="../../index.html"><img src="./webroot/images/home.png" alt="home"></a>
+            <h3 class="text-white">ProyectoDWES - Tema 03</h3>
+        </nav>
+        <table class="table table-striped">
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_00.php">Ejercicio 00.- Hola mundo y phpinfo().</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_01.php">Ejercicio 01.- Inicializar variables de los distintos tipos de datos básicos(string, int, float, bool) y mostrar los datos por pantalla (echo, print, printf,
+                        print_r, var_dump).</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_02.php">Ejercicio 02.- Inicializar y mostrar una variable heredoc.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_03.php">Ejercicio 03.- Mostrar en tu página index la fecha y hora actual formateada en castellano.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_04.php">Ejercicio 04.- Mostrar en tu página index la fecha y hora actual en Oporto formateada en portugués.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_05.php">Ejercicio 05.- Inicializar y mostrar una variable que tiene una marca de tiempo (timestamp)</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_06.php">Ejercicio 06.- Operar con fechas: calcular la fecha y el día de la semana de dentro de 60 días.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_07.php">Ejercicio 07.- Mostrar el nombre del fichero que se está ejecutando.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_08.php">Ejercicio 08.- Mostrar la dirección IP del equipo desde el que estás accediendo.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_09.php">Ejercicio 09.- Mostrar el path donde se encuentra el fichero que se está ejecutando.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_10.php">Ejercicio 10.- Mostrar el contenido del fichero que se está ejecutando.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_11.php">Ejercicio 11.- Mostrar el documento PHPDoc del proyecto que se está ejecutando generado con PHP Documentor o ApiGen.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_12.php">Ejercicio 12.- Mostrar el contenido de las variables superglobales (utilizando print_r() y foreach()).</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_13.php">Ejercicio 13.- Crear una función que cuente el número de visitas a la página actual desde una fecha concreta.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_14.php">Ejercicio 14.- Comprobar las librerías que estás utilizando en tu entorno de desarrollo y explotación. Crear tu propia librería de funciones y
+                        estudiar la forma de usarla en el entorno de desarrollo y en el de explotación.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_15.php">Ejercicio 15.- Crear e inicializar un array con el sueldo percibido de lunes a domingo. Recorrer el array para calcular el sueldo percibido durante
+                        la semana. (Array asociativo con los nombres de los días de la semana).</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_16.php">Ejercicio 16.- Recorrer el array anterior utilizando funciones para obtener el mismo resultado.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_17.php">Ejercicio 17.- Inicializar un array (bidimensional con dos índices numéricos) donde almacenamos el nombre de las personas que tienen reservado
+                        el asiento en un teatro de 20 filas y 15 asientos por fila. (Inicializamos el array ocupando únicamente 5 asientos). Recorrer el array
+                        con distintas técnicas (foreach(), while(), for()) para mostrar los asientos ocupados en cada fila y las personas que lo ocupan.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_18.php">Ejercicio 18.- Recorrer el array anterior utilizando funciones para obtener el mismo resultado.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_19.php">Ejercicio 19.- Construir una librería de funciones de validación de campos de formularios (LibreríaValidacionFormularios.php) para utilizarla en
+                        los siguientes ejercicios.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_20.php">Ejercicio 20.- Convertir la LibreriaValidacionFormularios.php en una clase ValidacionFormularios.php.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_21.php">Ejercicio 21.- Construir un formulario para recoger un cuestionario realizado a una persona y enviarlo a una página Tratamiento.php para que
+                        muestre las preguntas y las respuestas recogidas.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_22.php">Ejercicio 22.- Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las
+                        respuestas recogidas.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_23.php">Ejercicio 23.- Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las
+                        respuestas recogidas; en el caso de que alguna respuesta esté vacía o errónea volverá a salir el formulario con el mensaje
+                        correspondiente.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_24.php">Ejercicio 24.- Construir un formulario para recoger un cuestionario realizado a una persona y mostrar en la misma página las preguntas y las
+                        respuestas recogidas; en el caso de que alguna respuesta esté vacía o errónea volverá a salir el formulario con el mensaje
+                        correspondiente, pero las respuestas que habíamos tecleado correctamente aparecerán en el formulario y no tendremos que volver a
+                        teclearlas.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_25.php">Ejercicio 25.- Trabajar en PlantillaFormulario.php mi plantilla para hacer formularios como churros.</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <a href="./codigoPHP/Ejercicio_26.php">Ejercicio 26.- Probar la plantilla anterior desarrollando un formulario que recoja la temperatura y la presión atmosférica en una serie de fechas y
+                        (cuando el usuario lo decida) genere un informe con los datos recibidos y un promedios, mínimos y máximos de temperatura y
+                        presión atmosférica.</a>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
+
